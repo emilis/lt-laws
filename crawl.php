bd3b70454038729eb6281b151ac755e9c3a20748
Emilis Dambauskas <emilis.d@gmail.com>
2010-10-28 11:10:59 +0300

    Added saving of pages.

--- crawl.php
+++ crawl.php
@@ -1,9 +1,11 @@
 <?php
 
+$dir = dirname(__FILE__);
 $url = "http://www3.lrs.lt/pls/inter3/dokpaieska.rezult_l?p_drus=102&p_gal=33&p_no=";
 
 for ($i=1;$i<173;$i++) {
+    echo "$i\n";
     $contents = file_get_contents("$url$i");
-
+    file_put_contents("$dir/contents/$i.html", $contents);
     sleep(2);
 }
