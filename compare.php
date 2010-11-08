38913eb648a589c6f80bdc542bde3b80d497c6a7
Emilis Dambauskas <emilis.d@gmail.com>
2010-11-08 14:09:30 +0200

    Added more parameters to compare script frames.

--- compare.php
+++ compare.php
@@ -1,14 +1,20 @@
+<?php
+
+$left = array_key_exists("left", $_GET) ? $_GET["left"] : "";
+$right = array_key_exists("right", $_GET) ? $_GET["right"] : "";
+
+?><!DOCTYPE html>
 <html>
 <head>
     <title>
 </head>
 <frameset cols="50%,50%">
-    <frameset rows="120,*">
-        <frame src="doc-chooser.php?side=left"></frame>
-        <frame name="doc-left" src="about:blank"></frame>
+    <frameset rows="110,*">
+        <frame src="doc-chooser.php?side=left&url=<?php echo urlencode($left); ?>"></frame>
+        <frame name="doc-left" src="<?php echo htmlspecialchars($left); ?>"></frame>
     </frameset>
-    <frameset rows="120,*">
-        <frame src="doc-chooser.php?side=right"></frame>
-        <frame name="doc-right" src="about:blank"></frame>
+    <frameset rows="110,*">
+        <frame src="doc-chooser.php?side=right&url=<?php echo urlencode($right); ?>"></frame>
+        <frame name="doc-right" src="<?php echo htmlspecialchars($right); ?>"></frame>
     </frameset>
 </frameset>      
--- doc-chooser.php
+++ doc-chooser.php
@@ -1,25 +1,47 @@
+<?php
+
+if (array_key_exists("url", $_GET))
+    $url = $_GET["url"];
+else
+    $url = "";
+
+?><!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 <body>
 <form method="POST" action="go.php" target="doc-<?php echo $_GET["side"]; ?>">
-<input name="user-url" style="width: 95%">
+<input id="user-url" name="user-url" style="width: 95%" value="<?php echo  $url; ?>">
 <select name="url">
-<option value=""></option>
+<option value="">Pasirinkite įstatymą...</option>
 <?php
 
-$laws = json_decode(file_get_contents(dirname(__FILE__) . "/docs.json"));
+$laws = json_decode(file_get_contents(dirname(__FILE__) . "/laws.json"));
 
-foreach ($laws as $law) {
-    echo '<option value="' . $law->url . '">';
-    echo htmlspecialchars($law->title);
+foreach ($laws->data as $law) {
+    echo '<option value="' . $law[1] . '"';
+    if ($url == $law[1])
+        echo ' selected';
+    echo '>';
+    echo htmlspecialchars($law[2]);
     echo '</option>' . "\n";
 }
 
 ?>
 </select>
-<input type="submit" value="show">
+<input type="submit" value="rodyti">
 </form>
+
+<script type="text/javascript">
+var url = document.getElementById("user-url");
+if (!url.value) {
+    url.value = "Įveskite adresą...";
+    url.onfocus = function() {
+        url.value = "";
+        url.onfocus = undefined;
+    }
+}
+</script>
 </body>
 </html>
