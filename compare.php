86eb04e5251f6a6b889cdac3e74db2ee8e7755b1
Emilis Dambauskas <emilis.d@gmail.com>
2010-11-08 09:46:19 +0200

    Renamed diff.php to compare.php for clarity.

--- compare.php
+++ compare.php
@@ -4,11 +4,11 @@
 </head>
 <frameset cols="50%,50%">
     <frameset rows="120,*">
-        <frame src="diff-selector.php?side=left"></frame>
+        <frame src="doc-chooser.php?side=left"></frame>
         <frame name="doc-left" src="about:blank"></frame>
     </frameset>
     <frameset rows="120,*">
-        <frame src="diff-selector.php?side=right"></frame>
+        <frame src="doc-chooser.php?side=right"></frame>
         <frame name="doc-right" src="about:blank"></frame>
     </frameset>
 </frameset>      
